b6aa653a785670c639751e4b08c67301f6adf52e
Iima <earl790314@gmail.com>
2024-05-10 11:52:24 +0800

    Revise create table SQL

--- dags/proj_city_dashboard/simple_template/create_table_sql.sql
+++ dags/proj_city_dashboard/simple_template/create_table_sql.sql
@@ -1,35 +1,16 @@
-DROP TABLE IF EXISTS public.heal_hospital;
-DROP TRIGGER IF EXISTS heal_hospital_mtime ON public.heal_hospital;
-DROP SEQUENCE IF EXISTS public.heal_hospital_ogc_fid_seq;
-
-    -- create sequnce
-    CREATE SEQUENCE IF NOT EXISTS public.heal_hospital_ogc_fid_seq
-        INCREMENT 1
-        START 1
-        MINVALUE 1
-        MAXVALUE 9223372036854775807
-        CACHE 1;
-
-    -- grant sequnce
-    ALTER TABLE IF EXISTS public.heal_hospital_ogc_fid_seq OWNER to postgres;
-    GRANT ALL ON TABLE public.heal_hospital_ogc_fid_seq TO postgres WITH GRANT OPTION;
+DROP TABLE IF EXISTS public.test;
+DROP TRIGGER IF EXISTS test_mtime ON public.test;
+DROP SEQUENCE IF EXISTS public.test_ogc_fid_seq;
 
     -- create table
-    CREATE TABLE IF NOT EXISTS public.heal_hospital
+    CREATE TABLE IF NOT EXISTS public.test
     (
                 data_time timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
         name character varying(50) COLLATE pg_catalog."default",
         addr text COLLATE pg_catalog."default",
         lng double precision,
         lat double precision,
-        wkb_geometry geometry(Point,4326),
-        _ctime timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
-        _mtime timestamp with time zone DEFAULT CURRENT_TIMESTAMP
-            ,
-        ogc_fid integer NOT NULL DEFAULT nextval('heal_hospital_ogc_fid_seq'::regclass),
-
-        CONSTRAINT heal_hospital_pkey PRIMARY KEY (ogc_fid)
-
+        wkb_geometry geometry(Point,4326)
     )
     WITH (
         OIDS = FALSE
@@ -37,48 +18,22 @@ DROP SEQUENCE IF EXISTS public.heal_hospital_ogc_fid_seq;
     TABLESPACE pg_default;
 
     -- grant table
-    ALTER TABLE IF EXISTS public.heal_hospital OWNER to postgres;
-    GRANT ALL ON TABLE public.heal_hospital TO postgres WITH GRANT OPTION;
+    ALTER TABLE IF EXISTS public.test OWNER to postgres;
+    GRANT ALL ON TABLE public.test TO postgres WITH GRANT OPTION;
 
-    -- create mtime trigger
-    CREATE TRIGGER heal_hospital_mtime
-        BEFORE INSERT OR UPDATE
-        ON public.heal_hospital
-        FOR EACH ROW
-        EXECUTE PROCEDURE public.trigger_set_timestamp();
-
-DROP TABLE IF EXISTS public.heal_hospital_history;
-DROP TRIGGER IF EXISTS heal_hospital_history_mtime ON public.heal_hospital_history;
-DROP SEQUENCE IF EXISTS public.heal_hospital_history_ogc_fid_seq;
-
-    -- create sequnce
-    CREATE SEQUENCE IF NOT EXISTS public.heal_hospital_history_ogc_fid_seq
-        INCREMENT 1
-        START 1
-        MINVALUE 1
-        MAXVALUE 9223372036854775807
-        CACHE 1;
-
-    -- grant sequnce
-    ALTER TABLE IF EXISTS public.heal_hospital_history_ogc_fid_seq OWNER to postgres;
-    GRANT ALL ON TABLE public.heal_hospital_history_ogc_fid_seq TO postgres WITH GRANT OPTION;
+DROP TABLE IF EXISTS public.test_history;
+DROP TRIGGER IF EXISTS test_history_mtime ON public.test_history;
+DROP SEQUENCE IF EXISTS public.test_history_ogc_fid_seq;
 
     -- create table
-    CREATE TABLE IF NOT EXISTS public.heal_hospital_history
+    CREATE TABLE IF NOT EXISTS public.test_history
     (
                 data_time timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
         name character varying(50) COLLATE pg_catalog."default",
         addr text COLLATE pg_catalog."default",
         lng double precision,
         lat double precision,
-        wkb_geometry geometry(Point,4326),
-        _ctime timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
-        _mtime timestamp with time zone DEFAULT CURRENT_TIMESTAMP
-            ,
-        ogc_fid integer NOT NULL DEFAULT nextval('heal_hospital_history_ogc_fid_seq'::regclass),
-
-        CONSTRAINT heal_hospital_history_pkey PRIMARY KEY (ogc_fid)
-
+        wkb_geometry geometry(Point,4326)
     )
     WITH (
         OIDS = FALSE
@@ -86,12 +41,5 @@ DROP SEQUENCE IF EXISTS public.heal_hospital_history_ogc_fid_seq;
     TABLESPACE pg_default;
 
     -- grant table
-    ALTER TABLE IF EXISTS public.heal_hospital_history OWNER to postgres;
-    GRANT ALL ON TABLE public.heal_hospital_history TO postgres WITH GRANT OPTION;
-
-    -- create mtime trigger
-    CREATE TRIGGER heal_hospital_history_mtime
-        BEFORE INSERT OR UPDATE
-        ON public.heal_hospital_history
-        FOR EACH ROW
-        EXECUTE PROCEDURE public.trigger_set_timestamp();
+    ALTER TABLE IF EXISTS public.test_history OWNER to postgres;
+    GRANT ALL ON TABLE public.test_history TO postgres WITH GRANT OPTION;
